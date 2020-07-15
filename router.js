7b3a135a034e92a8652882a300732fb2bdeed3a8
Cauan <unconfigured@null.spigotmc.org>
2020-07-15 13:38:49 -0300

    removido middleware de cors

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 
 const router = express.Router();
 
+const verifyJWT = require('./middleware')
 const get = require('./rotas/get')
 const put = require('./rotas/put')
 const post = require('./rotas/post')
@@ -10,25 +11,19 @@ const delete_req = require('./rotas/delete')
 const decode = require('./rotas/decode')
 
 router.use(require('cors')());
-router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 
 //Rota: Listar usuarios
-router.get('/', get)
+router.get('/', verifyJWT,get)
 //Rota: Criação de usuario
-router.post('/', post)
+router.post('/', verifyJWT, post)
 //Rota: Listar usuario pelo ID
-router.get('/:id', get)
+router.get('/:id', verifyJWT,get)
 //Rota: Atualizar usuario
-router.put('/:id', put)
+router.put('/:id', verifyJWT,put)
 //Rota: Deletar usuario pelo ID
-router.delete('/:id', delete_req)
+router.delete('/:id', verifyJWT,delete_req)
 
 module.exports = router;
